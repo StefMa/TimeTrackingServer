43be46b566c086eaa5d7f19d10e6dded4dd71aaa
StefMa <StefMaDev@outlook.com>
2016-05-07 14:46:49 +0200

    Check header in creater user API
    See https://github.com/StefMa/TimeTrackingServer/issues/12

--- restapicreate.py
+++ restapicreate.py
@@ -21,22 +21,26 @@ class CreateHandler(webapp2.RequestHandler):
     json_string = json.loads(body)
     logging.info(json_string)
 
-    username = json_string["username"]
-    token = user_util.create_user(username)
-    if token is not None:
-        result = {
-            "username" : username,
-            "result" : "Ok",
-            "token" : token
-        }
+    h_helper = header_helper(self.request.headers)
+    if h_helper.valid_auth() and h_helper.valid_api_version() == "1":
+        username = json_string["username"]
+        token = user_util.create_user(username)
+        if token is not None:
+            result = {
+                "username" : username,
+                "result" : "Ok",
+                "token" : token
+            }
+        else:
+            result = {
+                "username" : username,
+                "result": "Already exist",
+                "token" : None
+            }
+        self.response.out.write(json.dumps(result))
+        self.response.set_status(200)
     else:
-        result = {
-            "username" : username,
-            "result": "Already exist",
-            "token" : None
-        }
-    self.response.out.write(json.dumps(result))
-    self.response.set_status(200)
+        self.response.set_status(401)
 
   def get(self):
     self.redirect("/")
